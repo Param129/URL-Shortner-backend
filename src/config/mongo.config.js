cd185401044cad978f55a001877de349054ba1f8
Param Tomar <paramtomar81@gmail.com>
2025-07-06 14:28:12 +0530

    connection timeout increased

--- src/config/mongo.config.js
+++ src/config/mongo.config.js
@@ -1,18 +1,33 @@
 import mongoose from "mongoose"
 
 const connectDB = async() => {
-    try{
+    try {
+        // Set global mongoose options
+        mongoose.set('bufferCommands', false); // Disable command buffering
+        
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
-            socketTimeoutMS: 45000, // Socket timeout
-            connectTimeoutMS: 30000 // Connection timeout
+            serverSelectionTimeoutMS: 60000, // Increase to 60 seconds
+            socketTimeoutMS: 75000, // Increase socket timeout
+            connectTimeoutMS: 60000, // Increase connection timeout
+            maxPoolSize: 10, // Control connection pool size
+            minPoolSize: 2, // Maintain minimum connections
+            maxIdleTimeMS: 30000 // Close idle connections after 30 seconds
+        });
+        
+        // Add connection event listeners
+        mongoose.connection.on('error', (err) => {
+            console.error('MongoDB connection error:', err);
+        });
+        
+        mongoose.connection.on('disconnected', () => {
+            console.log('MongoDB disconnected, attempting to reconnect...');
         });
         
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     }
-    catch(err){
+    catch(err) {
         console.error(`MongoDB Connection Error: ${err.message}`);
         process.exit(1);
     }
