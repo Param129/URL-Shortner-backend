6d36ea5c4da267d7e3f098138acde8a807bbcc84
Param Tomar <paramtomar81@gmail.com>
2025-07-06 16:09:12 +0530

    cookie setting changed

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,14 @@
 
+// export const cookieOptions = {
+//     httpOnly: true,
+//     secure: process.env.NODE_ENV === "production",
+//     sameSite: "Lax",
+//     maxAge: 1000 * 60 * 60 * 24 *7
+// }
+
 export const cookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "Lax",
-    maxAge: 1000 * 60 * 60 * 24 *7
+    secure: true, // Render should serve over HTTPS
+    sameSite: "None", // Needed for cross-site cookies
+    maxAge: 1000 * 60 * 60 * 24 * 7
 }
