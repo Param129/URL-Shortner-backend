bbdf4989b14c17bdea1e9da5a9c76d47fe0eca9b
Param Tomar <paramtomar81@gmail.com>
2025-07-06 15:37:18 +0530

    add cookiesetting

--- app.js
+++ app.js
@@ -16,20 +16,22 @@ import { attachUser } from "./src/utils/attachUser.js";
 const app = express();
 
 const allowedOrigins = [
-    'http://localhost:5173',
-    'https://url-shortener-frontend-gilt.vercel.app',
-  ];
-  
-  app.use(cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-  }));
+  'http://localhost:5173',
+  'https://url-shortener-frontend-gilt.vercel.app',
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
